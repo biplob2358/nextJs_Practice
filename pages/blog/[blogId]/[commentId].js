3ab8fe7aef292f1df8c2a1e814fa0b274962d2ce
unknown <biplob2358@gmail.com>
2023-03-09 20:06:24 +0600

    Dynamic nesting  routes

---
+++ pages/blog/[blogId]/[commentId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CommentDetails() {
+    return (
+        <div>
+            <h1>Comment Details</h1>
+        </div>
+    );
+}
+
+export default CommentDetails;
---
+++ pages/blog/[blogId]/index.js
@@ -0,0 +1,11 @@
+
+
+function index() {
+    return (
+        <div>
+            <h1>This is comment page</h1>
+        </div>
+    );
+}
+
+export default index;
